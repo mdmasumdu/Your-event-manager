9e076b4ce1a30260688300fac315feaf5f67897d
MD MASUM <mdmasumdu45@gmail.com>
2023-10-08 13:55:10 +0600

    fixed nav reloading issue

--- src/Mainlayout/Mainlayout.jsx
+++ src/Mainlayout/Mainlayout.jsx
@@ -1,9 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Nav from "../Navbar/Nav";
 import Footer from "../Pages/Footer/Footer";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+
 
 
 const Mainlayout = () => {
+    useEffect(() => {
+        AOS.init();
+      }, []);
     return (
         <div className="bg-slate-200 text-black container mx-auto ">
             <Nav></Nav>
